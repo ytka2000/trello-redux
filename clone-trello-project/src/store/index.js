ac5ee12d0633ca8144440f91d714f349ad752d53
Oleksandra Nedashkivska <cas10ned@gmail.com>
2022-06-02 17:14:03 +0300

    add redux dev tool extension

--- clone-trello-project/src/store/index.js
+++ clone-trello-project/src/store/index.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import thunk from "redux-thunk";
 import cardReducer from "./cardReducer";
 import columnReducer from "./columnReducer";
@@ -8,6 +9,9 @@ const rootReducer = combineReducers({
 	columnsObj: columnReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
